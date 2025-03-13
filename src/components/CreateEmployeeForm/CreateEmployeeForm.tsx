fe3e6146a608a4d3fec3b911d9101c403cc71b3f
axel husson <aaaxel.hsson@gmail.com>
2025-03-13 16:26:11 +0100

    add error message on submit

--- src/components/CreateEmployeeForm/CreateEmployeeForm.tsx
+++ src/components/CreateEmployeeForm/CreateEmployeeForm.tsx
@@ -1,21 +1,26 @@
-
-import { useForm} from '@tanstack/react-form'
-
+import { useForm } from "@tanstack/react-form";
 
 export default function CreateEmployeeForm() {
   const form = useForm({
     defaultValues: {
       firstName: "",
       lastName: "",
-    }, 
-    onSubmit: ({value}) => {
+    },
+    onSubmit: ({ value }) => {
       console.log("values", value);
-    }
-  })
+    },
+  });
   return (
     <>
       <div>CreateEmployeeForm</div>
       <form.Field
+        validators={{
+          onSubmit: ({ value }) => {
+            if (value.length < 3) {
+              return "First Name must be at least 3 characters";
+            }
+          },
+        }}
         name="firstName"
         children={(field) => (
           <div>
@@ -27,11 +32,21 @@ export default function CreateEmployeeForm() {
               value={field.state.value}
               onChange={(e) => field.handleChange(e.target.value)}
             />
+            {field.state.meta.errors && (
+              <div className="text-red-300">{field.state.meta.errors}</div>
+            )}
           </div>
         )}
       />
       <form.Field
         name="lastName"
+        validators={{
+          onSubmit: ({ value }) => {
+            if (value.length < 3) {
+              return "Last Name must be at least 3 characters";
+            }
+          },
+        }}
         children={(field) => (
           <div>
             <label htmlFor="lastName">Last Name : </label>
@@ -42,11 +57,18 @@ export default function CreateEmployeeForm() {
               value={field.state.value}
               onChange={(e) => field.handleChange(e.target.value)}
             />
+            {field.state.meta.errors && (
+              <span className="text-red-300">{field.state.meta.errors}</span>
+            )}
           </div>
         )}
       />
-      <button type='submit' onClick={form.handleSubmit}>Submit Button</button>
-      <button type='button' onClick={() => form.reset()}>Reset Button</button>
+      <button type="submit" onClick={form.handleSubmit}>
+        Submit Button
+      </button>
+      <button type="button" onClick={() => form.reset()}>
+        Reset Button
+      </button>
     </>
   );
 }
