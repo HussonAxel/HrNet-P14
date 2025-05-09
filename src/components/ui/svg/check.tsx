16a1f8525146f00006a2a2875a3c5fca3daefae5
axel husson <aaaxel.hsson@gmail.com>
2025-05-09 12:09:41 +0200

    Refactor button component and remove dialog component
    - Updated Button component to use forwardRef for better ref handling and added missing semicolons for consistency.
    - Removed Dialog component and its related exports to streamline the UI component library.
    - Refactored SVG components (CalendarSVG, CaretSVG, CheckSVG) to use forwardRef for improved ref support and added display names for better debugging.

--- src/components/ui/svg/check.tsx
+++ src/components/ui/svg/check.tsx
@@ -1,14 +1,21 @@
+import React from "react";
+
 interface CheckSVGProps extends React.SVGProps<SVGSVGElement> {}
 
-const CheckSVG: React.FC<CheckSVGProps> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={32}
-    height={32}
-    viewBox="0 0 256 256"
-    {...props}
-  >
-    <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path>
-  </svg>
+const CheckSVG = React.forwardRef<SVGSVGElement, CheckSVGProps>(
+  (props, ref) => (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path>
+    </svg>
+  )
 );
+CheckSVG.displayName = "CheckSVG";
+
 export default CheckSVG;
